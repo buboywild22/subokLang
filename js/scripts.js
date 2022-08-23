5ae6356749d553e49c4e80a7cc125876f2858249
Mark Destriza <mmjdestriza@gmail.com>
2022-08-23 16:31:01 +0800

    added javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
-var counter = 0;
-function add(){
-    counter++;
-    document.getElementById("counterId").innerHTML = counter;
+var cartArray = [];
+var totalQuantity = 0;
+var totalCartItems = 0;
+
+function item1(){
+    var itemquantity = parseInt(document.getElementById("item1value").value);
+    var price = 15000;
+    totalQuantity=+itemquantity;
+    var totalprice = itemquantity * price;
+    cartArray.push(totalprice);
+}
+function item2(){
+    var itemquantity = parseInt(document.getElementById("item2value").value);
+    var price = 20000;
+    totalQuantity=+itemquantity;
+    var totalprice = itemquantity * price;
+    cartArray.push(totalprice);
+}
+function item3(){
+    var itemquantity = parseInt(document.getElementById("item3value").value);
+    var price = 5000;
+    totalQuantity=+itemquantity;
+    var totalprice = itemquantity * price;
+    cartArray.push(totalprice);
+}
+
+function checkOut(){
+    for(var i=0;i<cartArray.length;i++){
+        totalCartItems=+cartArray[i];
+    }
+    console.log("The total price of all items:",totalCartItems);
 }
