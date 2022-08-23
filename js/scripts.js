feff5f8bb7d3e25c3b202a9a159916ca33fcd882
Mark Destriza <mmjdestriza@gmail.com>
2022-08-23 18:59:44 +0800

    added cart modal and js for user input

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,10 @@ function item1(){
     var itemquantity = parseInt(document.getElementById("item1value").value);
     var price = 15000;
     totalQuantity=+itemquantity;
-    var totalprice = itemquantity * price;
-    cartArray.push(totalprice);
+    var totalPrice = itemquantity * price;
+    cartArray.push({id:1,name:"A76",quantity:totalQuantity,currentitemprice:price,overallitemprice:totalPrice});
+    document.getElementById("counterId").innerHTML = totalQuantity;
+    //need code to hide modal after click
 }
 function item2(){
     var itemquantity = parseInt(document.getElementById("item2value").value);
@@ -24,9 +26,20 @@ function item3(){
     cartArray.push(totalprice);
 }
 
-function checkOut(){
-    for(var i=0;i<cartArray.length;i++){
-        totalCartItems=+cartArray[i];
+function displayItems(){
+    if(cartArray.length==0){
+        document.getElementById("checkOutTable").innerHTML = ""
     }
-    console.log("The total price of all items:",totalCartItems);
+    for(var i=0;i<cartArray.length;i++){
+        document.getElementById("checkOutTable").innerHTML += `
+        <tr>
+            <th scope="row">`+cartArray[i].id+`</th>
+            <td>`+cartArray[i].name+`</td>
+            <td><input type="text" class="form-control" id="item" value="`+cartArray[i].quantity+`"></td>
+            <td>`+cartArray[i].currentitemprice+`</td>
+            <td>`+cartArray[i].overallitemprice+`</td>
+        </tr>
+        `
+        console.log("ID:",cartArray[i].id,"Name:",cartArray[i].name,"Quantity:",cartArray[i].quantity)
+    }
 }
